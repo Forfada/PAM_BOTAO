e1c63b5c3fcf39c9f79dcd4ee2f6b47e7d645bbf
Forfada <162335523+Forfada@users.noreply.github.com>
2025-05-13 07:43:25 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Banco from './components/Banco'
+import Banco from './components/banco'
 
 export default function App() {
   return (
